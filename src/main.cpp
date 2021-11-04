c71270ea07e91ffcc892b906c22dae3f663b75d6
Kulverstukas <kulverstukas.k@gmail.com>
2021-11-04 17:19:08 +0200

    Padaryta labai daug dalykų ir iš esmės baigta, nes pamiršau komitinti iki šiol :D

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
 //---------- AP vars ----------
 #define AP_SSID "KMS Temp logger"
@@ -10,9 +14,20 @@ IPAddress AP_IP = IPAddress(10, 10, 10, 1);
 IPAddress AP_gateway = IPAddress(10, 10, 10, 1);
 IPAddress AP_nmask = IPAddress(255, 255, 255, 0);
 //-------- Program vars -------
-bool updateStatus = true;
-String responseMsg = String("23 25");
+#define SENSOR_1_PIN 5
+#define SENSOR_2_PIN 18
+float sensor1_temp;
+float sensor2_temp;
+float sensor1_comp;
+float sensor2_comp;
+long sensor1_millis;
+long sensor2_millis;
 AsyncWebServer server(80);
+OneWire sensor1_OW(SENSOR_1_PIN);
+OneWire sensor2_OW(SENSOR_2_PIN);
+DallasTemperature sensor1_DT(&sensor1_OW);
+DallasTemperature sensor2_DT(&sensor2_OW);
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 //-----------------------------
 String readFile(fs::FS &fs, const char * path) {
   Serial.println();
@@ -50,79 +65,186 @@ void writeFile(fs::FS &fs, const char * path, const char * message) {
 String processor(const String& var) {
   if (var == "ssid") {
     return readFile(SPIFFS, "/ssid.txt");
-  } else if (var == "passwd"){
+  } else if (var == "passwd") {
     return readFile(SPIFFS, "/passwd.txt");
+  } else if (var == "sens1") {
+    return readFile(SPIFFS, "/sensor1.txt");
+  } else if (var == "sens2") {
+    return readFile(SPIFFS, "/sensor2.txt");
   }
   return String();
 }
 //-----------------------------
-
+void readTemp_s1() {
+  float tmp = sensor1_DT.getTempCByIndex(0);
+  sensor1_temp = tmp;
+  if (sensor1_temp != DEVICE_DISCONNECTED_C) sensor1_temp += sensor1_comp;
+  // Serial.print("S1: ");
+  // Serial.println(sensor1_temp);
+  sensor1_DT.requestTemperatures();
+  sensor1_millis = millis();
+}
+//-----------------------------
+void readTemp_s2() {
+  float tmp = sensor2_DT.getTempCByIndex(0);
+  sensor2_temp = tmp;
+  if (sensor2_temp != DEVICE_DISCONNECTED_C) sensor2_temp += sensor2_comp;
+  // Serial.print("S2: ");
+  // Serial.println(sensor2_temp);
+  sensor2_DT.requestTemperatures();
+  sensor2_millis = millis();
+}
+//-----------------------------
+void updateS1OnScreen() {
+  String res;
+  if (sensor1_temp != DEVICE_DISCONNECTED_C) res += sensor1_temp;
+  else res += "XX.XX";
+  res += (char)223;
+  res += "C";
+  if (res.length() < 8) res += " ";
+  lcd.setCursor(0, 0);
+  lcd.print(res.c_str());
+  // if ((sensor1_temp != DEVICE_DISCONNECTED_C) && ((sensor1_temp > 00.00) || (sensor1_temp < 100.00))) lcd.print(" ");
+}
+//-----------------------------
+void updateS2OnScreen() {
+  String res;
+  if (sensor2_temp != DEVICE_DISCONNECTED_C) res += sensor2_temp;
+  else res += "XX.XX";
+  res += (char)223;
+  res += "C";
+  if (res.length() < 8) res += " ";
+  lcd.setCursor(8, 0);
+  lcd.print(res.c_str());
+  // lcd.setCursor(7, 0);
+  // if (sensor2_temp != DEVICE_DISCONNECTED_C) {
+  //   if ((sensor2_temp > 00.00) || (sensor2_temp < 99.99)) lcd.print(" ");
+  //   lcd.print(sensor2_temp);
+  // } else lcd.print("XX.XX");
+  // lcd.print((char)223);
+  // lcd.print("C");
+  // lcd.print(" ");
+}
+//-----------------------------
+void initApMode() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(AP_SSID);
+  delay(100); // crude hack
+  WiFi.softAPConfig(AP_IP, AP_gateway, AP_nmask);
+  Serial.print("AP IP Address: ");
+  Serial.println(WiFi.softAPIP());
+  lcd.setCursor(0, 1);
+  lcd.print("AP! ");
+  lcd.print(WiFi.softAPIP());
+}
+//-----------------------------
+void onFailedToConnect(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("Cannot connect to network!");
+  initApMode();
+}
+//-----------------------------
+void onGotIp(WiFiEvent_t event, WiFiEventInfo_t info) {
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  lcd.setCursor(0, 1);
+  lcd.print(ip);
+}
+//-----------------------------
 void setup() {
   Serial.begin(115200);
+
+  Wire.begin(21, 22);
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+
+  sensor1_DT.setWaitForConversion(false);
+  sensor1_DT.setResolution(0x1F);
+  sensor1_DT.begin();
+  sensor1_DT.requestTemperatures();
+  sensor1_millis = millis();
+
+  sensor2_DT.setWaitForConversion(false);
+  sensor2_DT.setResolution(0x1F);
+  sensor2_DT.begin();
+  sensor2_DT.requestTemperatures();
+  sensor2_millis = millis();
+
   if (!SPIFFS.begin()) {
-    updateStatus = false;
     Serial.println("SPIFFS Failed!");
-    // write on screen error msg here
+    lcd.setCursor(0, 1);
+    lcd.print("SPIFFS error");
   } else {
-    // try to connect to wifi
     WiFi.mode(WIFI_STA);
-    WiFi.begin(readFile(SPIFFS, "/ssid.txt").c_str(), readFile(SPIFFS, "/passwd.txt").c_str());
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-      Serial.println("Cannot connect to network!");
-      updateStatus = false;
-      // write on screen error msg here
-      WiFi.mode(WIFI_AP);
-      WiFi.softAP(AP_SSID);
-      WiFi.softAPConfig(AP_IP, AP_gateway, AP_nmask);
-      Serial.print("IP Address: ");
-      Serial.println(WiFi.softAPIP());
-
-      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(SPIFFS, "/index.html", "text/html", false, processor);
-      });
-
-      server.on("/save", HTTP_GET, [](AsyncWebServerRequest *request) {
-        String reqData;
-        if (request->hasParam("ssid")) {
-          reqData = request->getParam("ssid")->value();
-          writeFile(SPIFFS, "/ssid.txt", reqData.c_str());
-          Serial.print("Received ssid: ");
-          Serial.println(reqData);
-        }
-        if (request->hasParam("passwd")) {
-          reqData = request->getParam("passwd")->value();
-          writeFile(SPIFFS, "/passwd.txt", reqData.c_str());
-          Serial.print("Received passwd: ");
-          Serial.println(reqData);
-        }
-        request->redirect("/");
-      });
-
-      server.onNotFound([](AsyncWebServerRequest *request) {
-        request->send(404, "text/plain", "Not found");
-      });
-
-      server.begin();
-    } else {
-      updateStatus = true;
-      IPAddress ip = WiFi.localIP();
-      Serial.print("IP Address: ");
-      Serial.println(ip);
-      // write on screen info here
-
-      server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(200, "text/html", responseMsg.c_str());
-      });
-
-      server.onNotFound([](AsyncWebServerRequest *request) {
-        request->send(404, "text/plain", "Not found");
-      });
-
-      server.begin();
-    }
+    WiFi.onEvent(onFailedToConnect, SYSTEM_EVENT_STA_DISCONNECTED);
+    WiFi.onEvent(onGotIp, SYSTEM_EVENT_STA_GOT_IP);
+    String ssid = readFile(SPIFFS, "/ssid.txt");
+    if (ssid.isEmpty()) initApMode();
+    else WiFi.begin(ssid.c_str(), readFile(SPIFFS, "/passwd.txt").c_str());
   }
+
+  sensor1_comp = readFile(SPIFFS, "/sensor1.txt").toFloat();
+  sensor2_comp = readFile(SPIFFS, "/sensor2.txt").toFloat();
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/index.html", "text/html", false, processor);
+  });
+
+  server.on("/savenetwork", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String reqData;
+    if (request->hasParam("ssid")) {
+      reqData = request->getParam("ssid")->value();
+      writeFile(SPIFFS, "/ssid.txt", reqData.c_str());
+      Serial.print("Received ssid: ");
+      Serial.println(reqData);
+    }
+    if (request->hasParam("passwd")) {
+      reqData = request->getParam("passwd")->value();
+      writeFile(SPIFFS, "/passwd.txt", reqData.c_str());
+      Serial.print("Received passwd: ");
+      Serial.println(reqData);
+    }
+    request->redirect("/");
+  });
+
+  server.on("/savecalib", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String reqData;
+    if (request->hasParam("sens1")) {
+      reqData = request->getParam("sens1")->value();
+      writeFile(SPIFFS, "/sensor1.txt", reqData.c_str());
+      Serial.print("Received sensor 1: ");
+      Serial.println(reqData);
+    }
+    if (request->hasParam("sens2")) {
+      reqData = request->getParam("sens2")->value();
+      writeFile(SPIFFS, "/sensor2.txt", reqData.c_str());
+      Serial.print("Received sensor 2: ");
+      Serial.println(reqData);
+    }
+    request->redirect("/");
+  });
+
+  server.on("/gettemp", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String temps = String(sensor1_temp)+" "+String(sensor2_temp);
+    request->send(200, "text/html", temps.c_str());
+  });
+
+  server.onNotFound([](AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+  });
+
+  server.begin();
 }
 
 void loop() {
-  
+  if ((millis() - sensor1_millis) > 1000) {
+    readTemp_s1();
+    updateS1OnScreen();
+  }
+
+  if ((millis() - sensor2_millis) > 1000) {
+    readTemp_s2();
+    updateS2OnScreen();
+  }
 }
